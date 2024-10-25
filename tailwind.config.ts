b9a8e664e231a3e0b5076fe25f6c94850a3067a6
Faried Risky <fariedrisky@gmail.com>
2024-10-25 12:28:23 +0700

    style(sidebar): adjust icon colors, active item color, hover background color, and add custom Tailwind classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,11 @@ const config: Config = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      colors: {
+        'red-active': '#DB1430',
+        'dark-hover': '#2f2e3c',
+        'dark-icon': '#3a3a5d',
+      },
       animation: {
         "spin-slow": "spin 3s linear infinite",
         "spin-reverse": "spin 1s reverse infinite",
